43c6f360a6a91d26c26ef0b069abf8d241c7dc31
Manjunath <122592837+manjunath70360@users.noreply.github.com>
2024-03-14 13:25:41 +0530

    Update index.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,21 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import { Table } from 'react-bootstrap';
+import {useState, useEffect} from 'react'
+import {Table} from 'react-bootstrap'
 
-import './index.css'; 
+import './index.css'
 
 const Home = () => {
-  const [jokes, setJokes] = useState([]);
+  const [jokes, setJokes] = useState([])
 
   useEffect(() => {
-    fetch('https://v2.jokeapi.dev/joke/any?format=json&blacklistFlags=nsfw,sexist&type=single&lang=EN&amount=10')
-      .then((response) => response.json())
-      .then((data) => setJokes(data.jokes))
-      .catch((error) => console.error('Error fetching jokes:', error));
-  }, []);
+    fetch(
+      'https://v2.jokeapi.dev/joke/any?format=json&blacklistFlags=nsfw,sexist&type=single&lang=EN&amount=10',
+    )
+      .then(response => response.json())
+      .then(data =>
+        setJokes(data.jokes.map((joke, index) => ({...joke, id: index}))),
+      )
+      .catch(error => console.error('Error fetching jokes:', error))
+  }, [])
 
   return (
     <div>
-      <h2 className='heading'>Jokes</h2>
+      <h2 className="heading">Jokes</h2>
       <Table striped bordered hover responsive>
         <thead>
           <tr>
@@ -24,16 +28,16 @@ const Home = () => {
           </tr>
         </thead>
         <tbody>
-          {jokes.map((joke, index) => (
-            <tr key={index}>
-              <td>{index + 1}</td>
+          {jokes.map(joke => (
+            <tr key={joke.id}>
+              <td>{joke.id + 1}</td>
               <td>{joke.joke}</td>
             </tr>
           ))}
         </tbody>
       </Table>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
